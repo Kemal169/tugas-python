6ecbacd67fe461cd534a83fcb46ef21484391718
Kemal169 <104721199+Kemal169@users.noreply.github.com>
2023-10-24 13:35:13 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 
 # Baca data dari file CSV
-data = pd.read_csv("nama_file.csv")  # Ganti "nama_file.csv" dengan nama file CSV Anda
+data = pd.read_csv("data_infeksi_covid19_indonesia.csv")  # Ganti "nama_file.csv" dengan nama file CSV Anda
 
 # Tampilkan data di aplikasi Streamlit
 st.title("Aplikasi Streamlit untuk Data CSV")
