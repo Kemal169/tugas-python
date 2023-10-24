69a822e9e97e5723e67901bd05e29c1e09227b00
Kemal169 <104721199+Kemal169@users.noreply.github.com>
2023-10-24 13:39:36 +0700

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -5,6 +5,6 @@ import pandas as pd
 data = pd.read_csv("data_infeksi_covid19_indonesia.csv")  # Ganti "nama_file.csv" dengan nama file CSV Anda
 
 # Tampilkan data di aplikasi Streamlit
-st.title("Data infeksi Covid 19 Pada Tahun 20200")
+st.title("Data infeksi Covid 19 Pada Tahun 2020")
 st.write("Berikut adalah data dari file :")
 st.write(data)
